b984e56a2b1fa83456f929c10fca9b7f1e294a87
Arnaud <arnaud.stephan@unil.ch>
2018-03-20 18:29:22 +0100

    Ajout iFrame

--- YouTubeCollaborator/imports/ui/body.js
+++ YouTubeCollaborator/imports/ui/body.js
@@ -1,7 +1,27 @@
 //Importation des méthodes
 import { Template } from 'meteor/templating';
 
-//Importation des fichiers
+//Importation de body
 import './body.html';
+//Importation des templates
 import './templates/titres.html';
 import './templates/paragraphes.html';
+import './templates/YouTube.html';
+
+//Lancement de YouTube
+
+if (Meteor.isClient) {
+  onYouTubeIframeAPIReady = function() {
+    player = new YT.Player("player", {
+      height: "400",
+      width: "600",
+      videoId: "2bjk26RwjyU",
+      events: {
+        onReady: function (event) {
+          event.target.playVideo();
+        }
+      }
+    });
+  };
+  YT.load();
+}
