cf95bd15142eb35b66d8d4a9b46cd4f24bc0e91e
Arnaud <arnaud.stephan@unil.ch>
2018-03-20 17:45:43 +0100

    Test templates

--- YouTubeCollaborator/imports/ui/body.js
+++ YouTubeCollaborator/imports/ui/body.js
@@ -1,1 +1,7 @@
+//Importation des méthodes
+import { Template } from 'meteor/templating';
+
+//Importation des fichiers
 import './body.html';
+import './templates/titres.html';
+import './templates/paragraphes.html';
