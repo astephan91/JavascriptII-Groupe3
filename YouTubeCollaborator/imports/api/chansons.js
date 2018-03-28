9636d48722564b3c37202aa34478507166b7c1d9
Arnaud <arnaud.stephan@unil.ch>
2018-03-28 10:47:27 +0200

    Modif CSS et ajout de la db chansons

---
+++ YouTubeCollaborator/imports/api/chansons.js
@@ -0,0 +1,4 @@
+import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+
+export const Chansons = new Mongo.Collection('chansons');
--- YouTubeCollaborator/imports/ui/body.js
+++ YouTubeCollaborator/imports/ui/body.js
@@ -4,17 +4,18 @@ import { Template } from 'meteor/templating';
 //Importation de body
 import './body.html';
 //Importation des templates
-import './templates/titres.html';
-import './templates/paragraphes.html';
+import './templates/Header.html';
 import './templates/YouTube.html';
+import './templates/Search.html';
+import './templates/Parametres.html';
 
 //Lancement de YouTube
 
 if (Meteor.isClient) {
   onYouTubeIframeAPIReady = function() {
     player = new YT.Player("player", {
-      height: "400",
-      width: "600",
+      //height: "400",
+      //width: "600",
       videoId: "2bjk26RwjyU",
       events: {
         onReady: function (event) {
